55f49737a4999b667f4c97d9ca8f1b19c35b1a6a
Brandon Magill <bmagll@proton.me>
2025-12-07 22:22:33 -0600

    clean configuration.nix up

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, inputs,  ... }:
 
 {
@@ -19,24 +15,14 @@
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
+  
+  # Network Host Name
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
-  # Power Profiles Daemon
-  services.power-profiles-daemon.enable = true;
-  # Local Send
-  # Local Send
-  programs.localsend = {
-  enable = true;
-  openFirewall = true;  # opens TCP/UDP 53317 for you
-  };
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
@@ -59,8 +45,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   # XDG Portal Settings
- 
-  
   xdg.portal = {
     enable = true;
 
@@ -71,10 +55,6 @@
       # Make Hyprland the default portal
       config.common.default = [ "hyprland" "gtk" ];
     };
-
-    # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -93,16 +73,12 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Services
+
+  services.power-profiles-daemon.enable = true;
+
   services.greetd = {
     enable = true;
 
@@ -116,27 +92,20 @@
   };
 
   services.upower.enable = true;
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   
-  # 🌟 NEW SECTION: Enable Fingerprint Reader
   services.fprintd.enable = true;
   
-  # Optional: Also enable it for sudo authentication in the terminal
-  # Note: fprintd usually handles PAM configuration for login managers (like Greetd) automatically when enabled.
   security.pam.services = {
-    # 🌟 This section enables fingerprint authentication for Hyprlock
-    hyprlock = {
-    # You already had this one, but confirming it's there for terminal sudo
-    sudo.fprintAuth = true;
+     sudo.fprintAuth = true;
     };
   };
   
+  # User Info
   users.users.bmag = {
     isNormalUser = true;
     description = "Brandon";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    # thunderbird
     ];
   };
 
@@ -155,14 +124,19 @@
     };
   };
     
+  # Programs
+  programs.localsend = {
+    enable = true;
+    openFirewall = true;  # opens TCP/UDP 53317 for you
+  };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
   # Allow unfree packages
+
   nixpkgs.config.allowUnfree = true;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+
+  # Packages
   environment.systemPackages = with pkgs; [
   vim
   git
@@ -180,32 +154,7 @@
     nerd-fonts.jetbrains-mono
   ];
 };
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05";
 }
 
