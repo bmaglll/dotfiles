7484d60019f0968018db03a415ae45cb08def927
Brandon Magill <bmagll@proton.me>
2025-12-09 22:02:52 -0600

    Update NixOS config

--- hyprland/hyprlock.nix
+++ hyprland/hyprlock.nix
@@ -11,7 +11,7 @@
 
   background = {
     monitor = "eDP-1";
-    path = "/home/bmag/Pictures/wallpapers/ngc2899.png";
+    path = "/home/bmag/nixos-config/wallpapers/c691.jpg";
     blur_passes = 2;
     blur_size = 3;
   };
