698f870e92b3def33b9fa6303cb125e7133a322e
Brandon Magill <bmagll@proton.me>
2026-02-27 17:53:45 -0600

    Update NixOS config

--- home.nix
+++ home.nix
@@ -64,6 +64,10 @@
   keyMode = "vi";
   mouse = true;
   shortcut = "space"
+  extraConfig = ''
+  set -g mouse on
+  bind -n WheelUpPane if-shell -F -t = "#{mouse_any_flag}" "send-keys -M" "if -Ft= '#{pane_in_mode}' 'send-keys -M' 'copy-mode -e; send-keys -M'"
+  '';
   }
   ###########################################################################################
   # neovim
