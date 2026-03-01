b6b6d2a5ac8fbbbdca0563f0cc54b991c182891e
Brandon Magill <bmagll@proton.me>
2026-03-01 00:22:11 -0600

    Update NixOS config

---
+++ laptop_gpu.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  # AMD Phoenix APU - Hardware video acceleration
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      libva
+      libva-utils
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+  # Environment variables for Firefox VA-API support
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "radeonsi";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+}
