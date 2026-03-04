cce6f06df8ec779d33587c3fe4fbafb998516ed4
Brandon Magill <bmagll@proton.me>
2026-03-03 23:20:22 -0600

    Revert Wofi background to dark gray rgba(17,17,27,0.85)

--- home.nix
+++ home.nix
@@ -228,7 +228,7 @@
       /* Main window */
       window {
         font-family: "JetBrainsMono Nerd Font", monospace;
-        background-color: rgba(248, 250, 250, 0.85);
+        background-color: rgba(17, 17, 27, 0.85);
         border-radius: 12px;
         border: 2px solid rgba(205, 214, 244, 0.2);
       }
