0b484367e9655621d2b0343db3efe826d9318bb3
Brandon Magill <bmagll@proton.me>
2026-03-15 22:20:51 -0500

    add obs pkg

---
+++ hosts/desk-nix/configuration.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [
+      # Shared config
+      ../../common/configuration.nix
+      # Hardware (generate on tower with: nixos-generate-config)
+      ./hardware-configuration.nix
+    ];
+
+  # Hostname
+  networking.hostName = "desk-nix";
+}
