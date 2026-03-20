d8285c6d2ecbb4368354a7bab9ae60df6f622859
Brandon Magill <bmagll@proton.me>
2026-03-20 13:56:25 -0500

    rollback suspend/resume to original config

--- home.nix
+++ home.nix
@@ -142,8 +142,6 @@
     settings = {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session && systemctl --user stop hyprpaper.service";
-        after_sleep_cmd = "pkill -x hyprlock; sleep 2; systemctl --user restart hyprpaper.service; hyprctl dispatch dpms on; hyprlock";
         inhibit_if_fullscreen = true;
       };
 
