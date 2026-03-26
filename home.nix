d0f0b42bb45bfbeb09900608ffee2abd2bfdaed3
Brandon Magill <bmagll@proton.me>
2026-03-26 00:44:39 -0500

    add GTK4/GStreamer packages to home.nix, remove nix-shell wrapper from dashboard keybind

--- home.nix
+++ home.nix
@@ -54,7 +54,14 @@
       ps.anthropic
       ps.pygobject3
     ]))
-    # Added by Claude
+    # GTK4 + GStreamer (UniFi camera dashboard)
+    gtk4
+    gdk-pixbuf
+    gobject-introspection
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-rs
+    gst_all_1.gst-libav
   ];
   ###########################################################################################
   # Bash 
