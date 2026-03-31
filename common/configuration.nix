6726d8dc977ab260db220c622fcead92c30c2617
Brandon Magill <bmagll@proton.me>
2026-03-31 01:18:47 -0500

    yazi settings

--- common/configuration.nix
+++ common/configuration.nix
@@ -3,7 +3,6 @@
 {
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
