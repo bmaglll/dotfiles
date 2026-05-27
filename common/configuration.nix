1b17b7a9eb28abdf66aab27aeaeec02d2bb9d94b
Brandon Magill <bmagll@proton.me>
2026-05-27 17:23:19 -0500

    Enable bluetooth and blueman service

--- common/configuration.nix
+++ common/configuration.nix
@@ -54,6 +54,11 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
